eae44fd0f6e67a9266a2c54db7ba846b34297e4a
Igor Ferreira <igorcferreira@hotmail.com>
2016-02-16 00:22:46 +0000

    Demonstrate app test implementation

---
+++ CocoaHeadsApp/CocoaHeadsAppTests/MeetupAPITests.swift
@@ -0,0 +1,54 @@
+//
+//  MeetupAPITests.swift
+//  CocoaHeadsApp
+//
+//  Created by Igor Ferreira on 2/15/16.
+//  Copyright © 2016 CocoaHeads Brasil. All rights reserved.
+//
+
+import XCTest
+import Moya
+@testable import CocoaHeadsApp
+
+class MeetupAPITests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testMeetupAPI() {
+        let api:MeetupAPI = .ListEvents(perPage: 20, pageOffset: 0)
+        let baseURL = NSURL(string:"https://api.meetup.com/2")
+        
+        XCTAssertEqual(api.baseURL, baseURL)
+        XCTAssertEqual(api.path, "/events")
+        XCTAssertEqual(api.method, Moya.Method.GET)
+        
+        guard let apiParameters = api.parameters else {
+            XCTAssertTrue(false, "Parameters should be available")
+            return
+        }
+        
+        let parameters = ["sign":"true",
+            "photo-host":"public",
+            "page":"\(20)",
+            "offset":"\(0)",
+            "group_urlname":"AppsterdamBR",
+            "fields":["venue", "headcount", "photo_url"].joinWithSeparator(",")]
+        
+        for (key, value) in apiParameters {
+            guard let stringValue = value as? String else {
+                XCTAssertTrue(false, "\(key):\(value) is an invalid parameter")
+                return
+            }
+            XCTAssertEqual(parameters[key], stringValue)
+        }
+    }
+    
+}
