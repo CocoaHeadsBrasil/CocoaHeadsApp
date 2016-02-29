582c2d4ea88c84af3dc8855ba00b2b9b7b751975
Gustavo Barbosa <gustavocsb@gmail.com>
2016-02-28 19:09:02 -0500

    add models and their unit tests

---
+++ CocoaHeadsApp/CocoaHeadsApp/Classes/Model/Video.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+class Video {
+    
+    var id: Int?
+    var speakerId: Int?
+    var agendaId: Int?
+    var title: String?
+    var description: String?
+    var youtubeId: String?
+    var published: Bool?
+    var tags: [String] = []
+    
+    var youtubeURL: NSURL? {
+        guard let id = youtubeId else { return nil }
+        return NSURL(string: "https://www.youtube.com/watch?v=\(id)")
+    }
+    
+    init(dictionary: [String:AnyObject] = [:]) {
+        id = dictionary["id"] as? Int
+        speakerId = dictionary["palestrante_id"] as? Int
+        agendaId = dictionary["agenda_id"] as? Int
+        title = dictionary["titulo"] as? String
+        description = dictionary["descricao"] as? String
+        youtubeId = dictionary["youtube"] as? String
+        published = dictionary["published"] as? Bool
+        
+        if let commaSeparatedTags = dictionary["tags"] as? String {
+            tags = commaSeparatedTags.componentsSeparatedByString(",")
+                .map({ $0.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet()) })
+        }
+    }
+}
