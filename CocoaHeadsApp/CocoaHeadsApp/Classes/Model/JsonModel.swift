7d23c73cfb580a350ed364e0b4bc3c8ad98c5769
Gustavo Barbosa <gustavocsb@gmail.com>
2016-03-01 18:19:56 -0500

    models: move auxiliar extractor methods to its protocol extension

--- CocoaHeadsApp/CocoaHeadsApp/Classes/Model/JsonModel.swift
+++ CocoaHeadsApp/CocoaHeadsApp/Classes/Model/JsonModel.swift
@@ -4,40 +4,42 @@ protocol JSONParselable {
     static func withJSON(json: [String:AnyObject]) -> Self?
 }
 
-func number(input: [NSObject:AnyObject], key: String) -> NSNumber? {
-    return input[key] >>>= { $0 as? NSNumber }
-}
-
-func int(input: [NSObject:AnyObject], key: String) -> Int? {
-    return number(input, key: key).map { $0.integerValue }
-}
-
-func float(input: [NSObject:AnyObject], key: String) -> Float? {
-    return number(input, key: key).map { $0.floatValue }
-}
-
-func double(input: [NSObject:AnyObject], key: String) -> Double? {
-    return number(input, key: key).map { $0.doubleValue }
-}
-
-func string(input: [String:AnyObject], key: String) -> String? {
-    return input[key] >>>= { $0 as? String }
-}
-
-func bool(input: [String:AnyObject], key: String) -> Bool? {
-    return number(input, key: key).map { $0.boolValue }
-}
-
-func nsdate(input: [String:AnyObject], key: String) -> NSDate? {
-    guard let dateString = string(input, key: key) else { return nil }
-    let formatter = NSDateFormatter()
-    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
-    return formatter.dateFromString(dateString)
-}
-
-func url(input: [String:AnyObject], key: String) -> NSURL? {
-    guard let url = string(input, key: key) else { return nil }
-    return NSURL(string: url)
+extension JSONParselable {
+    static func number(input: [NSObject:AnyObject], key: String) -> NSNumber? {
+        return input[key] >>>= { $0 as? NSNumber }
+    }
+    
+    static func int(input: [NSObject:AnyObject], key: String) -> Int? {
+        return number(input, key: key).map { $0.integerValue }
+    }
+    
+    static func float(input: [NSObject:AnyObject], key: String) -> Float? {
+        return number(input, key: key).map { $0.floatValue }
+    }
+    
+    static func double(input: [NSObject:AnyObject], key: String) -> Double? {
+        return number(input, key: key).map { $0.doubleValue }
+    }
+    
+    static func string(input: [String:AnyObject], key: String) -> String? {
+        return input[key] >>>= { $0 as? String }
+    }
+    
+    static func bool(input: [String:AnyObject], key: String) -> Bool? {
+        return number(input, key: key).map { $0.boolValue }
+    }
+    
+    static func nsdate(input: [String:AnyObject], key: String) -> NSDate? {
+        guard let dateString = string(input, key: key) else { return nil }
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+        return formatter.dateFromString(dateString)
+    }
+    
+    static func url(input: [String:AnyObject], key: String) -> NSURL? {
+        guard let url = string(input, key: key) else { return nil }
+        return NSURL(string: url)
+    }
 }
 
 func flatten<A>(x: A??) -> A? {
