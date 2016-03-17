c8d117504d6867900afafd15bb8e2244c64f878d
viniciuscarvalho <viniciuscarvalho789@gmail.com>
2016-03-17 18:42:59 -0300

    Structure of project and add Controllers for Events

--- CocoaHeadsApp/CocoaHeadsApp/EventsViewController.swift
+++ CocoaHeadsApp/CocoaHeadsApp/EventsViewController.swift
@@ -1,24 +1,47 @@
 //
-//  EventsViewController.swift
+//  EventsDetailViewController.swift
 //  CocoaHeadsApp
 //
-//  Created by Antoine Barrault on 30/10/2015.
-//  Copyright © 2015 CocoaHeads Brasil. All rights reserved.
+//  Created by Vinicius Carvalho on 14/03/16.
+//  Copyright © 2016 CocoaHeads Brasil. All rights reserved.
 //
 
 import UIKit
 
-class EventsViewController: UIViewController {
+class EventsViewController: UITableViewController {
 
+    //private let events = Events.events
+    private let eventsDetailSegue = "eventsDetailSegue"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.registerNib(UINib(nibName: String(EventsTableViewCell), bundle: nil), forCellReuseIdentifier: String(EventsTableViewCell))
+        tableView.rowHeight = UITableViewAutomaticDimension
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(String(EventsTableViewCell), forIndexPath: indexPath) as! EventsTableViewCell
+        
+        //cell.configure(withEvents: events[indexPath.row])
+        
+        return cell
+    }
+
 
 
 }
