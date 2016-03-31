970d5c57050a21bbcab78b9ea16313f9278d1f96
viniciuscarvalho <viniciuscarvalho789@gmail.com>
2016-03-31 14:46:44 -0300

    Add elements for show Events List

---
+++ CocoaHeadsApp/CocoaHeadsApp/EventsListDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  EventsListDelegate.swift
+//  CocoaHeadsApp
+//
+//  Created by Vinicius Carvalho on 31/03/16.
+//  Copyright © 2016 CocoaHeads Brasil. All rights reserved.
+//
+
+import UIKit
