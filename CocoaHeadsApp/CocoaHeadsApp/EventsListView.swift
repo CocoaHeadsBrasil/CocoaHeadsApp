407de0a397df575b09d446cd083b58bd322d0303
Gustavo Barbosa <gustavocsb@gmail.com>
2016-06-18 18:25:54 -0400

    general: code cleanup

--- CocoaHeadsApp/CocoaHeadsApp/EventsListView.swift
+++ CocoaHeadsApp/CocoaHeadsApp/EventsListView.swift
@@ -1,11 +1,3 @@
-//
-//  EventsListView.swift
-//  CocoaHeadsApp
-//
-//  Created by Vinicius Carvalho on 31/03/16.
-//  Copyright © 2016 CocoaHeads Brasil. All rights reserved.
-//
-
 import UIKit
 
 class EventListView: NibDesignable {
