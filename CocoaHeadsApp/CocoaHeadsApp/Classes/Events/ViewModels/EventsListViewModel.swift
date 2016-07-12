8f1c53aaf5beb919667c4cf92648aaf4653f749d
Bruno Mazzo <mazzo.bruno@gmail.com>
2016-07-11 23:09:13 -0300

    Converting all Dynamic calls to RxSwift

--- CocoaHeadsApp/CocoaHeadsApp/Classes/Events/ViewModels/EventsListViewModel.swift
+++ CocoaHeadsApp/CocoaHeadsApp/Classes/Events/ViewModels/EventsListViewModel.swift
@@ -1,8 +1,9 @@
 import UIKit
+import RxSwift
 
 class EventsListViewModel: ViewModel {
     
     var numberOfItemsPerRequest = 20
     var currentPage = 0
-    let items = Dynamic<[Event]>([])
+    let items = Variable<[Event]>([])
 }
