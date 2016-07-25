5f9bcaf2700358b2014c7431ba8fafbefdb1f380
Gustavo Barbosa <gustavocsb@gmail.com>
2016-07-24 22:16:34 -0400

    chapters: update assets to correct sizes

---
+++ CocoaHeadsApp/Classes/Chapters/ViewModels/StatesListViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  StatesListViewModel.swift
+//  CocoaHeadsApp
+//
+//  Created by Gustavo Barbosa on 7/23/16.
+//  Copyright © 2016 CocoaHeads Brasil. All rights reserved.
+//
+
+import Foundation
---
+++ CocoaHeadsApp/Classes/Chapters/Views/StatesListTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  StatesListTableViewCell.swift
+//  CocoaHeadsApp
+//
+//  Created by Gustavo Barbosa on 7/24/16.
+//  Copyright © 2016 CocoaHeads Brasil. All rights reserved.
+//
+
+import Foundation
