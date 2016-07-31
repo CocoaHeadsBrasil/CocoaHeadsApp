b354f6b261e5e9cc89f946ccce3d4abe1271ea25
Gustavo Barbosa <gustavocsb@gmail.com>
2016-07-31 08:48:56 -0400

    general: fix tests

--- CocoaHeadsAppTests/AppCoordinatorTests.swift
+++ CocoaHeadsAppTests/AppCoordinatorTests.swift
@@ -26,42 +26,17 @@ class AppCoordinatorTests: QuickSpec {
                 it("should set the key window") {
                     expect(self.appCoordinator.window.keyWindow).to(beTrue())
                 }
-                
-                it("should return a Tabbar controller") {
-                    expect(returnController).to(beAKindOf(UITabBarController.self))
-                }
-                
+
                 it("should set the return viewController on the window") {
                     expect(self.appCoordinator.window.rootViewController) == returnController
                 }
-                
-                it("return tabbar should have two navigation controllers") {
-                    let tabbarController = returnController as! UITabBarController
-                    expect(tabbarController.viewControllers!.count) == 2
-                }
-                
-                it("return tabbar should have a MeetupViewController as first controller on a navigationController") {
-                    let tabbarController = returnController as! UITabBarController
-                    let navigationController = tabbarController.viewControllers![0] as! UINavigationController
-                    expect(navigationController.viewControllers[0]).to(beAKindOf(MeetupListViewController.self))
-                }
-                
-                it("return tabbar should have a SettingsViewController as second controller") {
-                    let tabbarController = returnController as! UITabBarController
-                    let navigationController = tabbarController.viewControllers![1 ] as! UINavigationController
-                    expect(navigationController.viewControllers[0]).to(beAKindOf(SettingsViewController.self))
-                }
-                
+
                 it("should create two children coordinator") {
-                    expect(self.appCoordinator.childrenCoordinator.count) == 2
+                    expect(self.appCoordinator.childrenCoordinator.count) == 1
                 }
-                
+
                 it("should create a meetupCoordinator") {
                     expect(self.appCoordinator.childrenCoordinator[0] as? MeetupCoordinator).toNot(beNil())
-                }
-                
-                it("should create a settingCoordinator") {
-                    expect(self.appCoordinator.childrenCoordinator[1] as? SettingsCoordinator).toNot(beNil())
                 }
             }
         }
